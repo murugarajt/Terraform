5981efc9a59a0fc7d425e4a4f6beaa7fc80f3d81
murugarajt <93312056+murugarajt@users.noreply.github.com>
2021-11-29 11:23:23 +0530

    Add files via upload

---
+++ Terraform-Gcp/terraform.tfvars
@@ -0,0 +1,7 @@
+network-name = "hu19-tf-vpc"
+region = "us-central1"
+subnet-name = "hu19-murugt-subnet"
+user = "raj"
+vmname ="hu19-muruga"
+zone = "us-central1-a"
+size = "e2-small"
---
+++ Terraform-Gcp/vriables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+    description = "name of deployment region"
+    type = string
+}
+
+variable "subnet-name"{
+    description = "name of subnet"
+    type = string
+}
+
+variable "network-name"{
+    description = "name of network"
+    type = string
+}
+
+variable "zone"{
+    description = "name of zone"
+    type = string
+}
+variable "vmname"{
+    description = "vm name"
+    type = string
+}
+variable "user"{
+    description = " vm username"
+    type = string
+}
